b1bbf04e94990b6419b76ce80a76fee9ede99de7
André Santanchè <santanche@gmail.com>
2024-10-05 10:50:00 -0300

    update (fastapi): double method

--- service/basic/fastapi/main.py
+++ service/basic/fastapi/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/double/{number}")
+def read_item(number: int | None = None):
+    return {"double": (number * 2)}
